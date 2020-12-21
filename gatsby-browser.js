9aed501dd98fea4820d75ac3db8d67209c4077bb
metruzanca <metruzanca@gmail.com>
2020-12-21 11:01:45 +0100

    Styling improvements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,4 +7,7 @@ import "./src/normalize.css"
 import "./src/style.css"
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+// import "prismjs/themes/prism.css"
+// import "./src/style/prism-synthwave84.css"
+// import "./src/style/prism-duotone-dark.css"
+import "./src/style/prism-atom-dark.css"
