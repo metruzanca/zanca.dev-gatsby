c51a1399a03351fc06febc62b2bd589ca4c6daaa
metruzanca <metruzanca@gmail.com>
2020-12-30 23:05:29 +0100

    Moved CSS files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,9 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
-import "./src/normalize.css"
+import "./src/style/normalize.css"
 // custom CSS styles
-import "./src/style.css"
+import "./src/style/style.css"
 
 // Highlighting for code blocks
 // import "prismjs/themes/prism.css"
