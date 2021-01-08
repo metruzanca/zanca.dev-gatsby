3aa45f4b41e53f558740fa0d9c55eab91461ca23
metruzanca <metruzanca@gmail.com>
2021-01-08 13:15:28 +0100

    pdf to doc and docx resume

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,10 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+require("dotenv").config({
+  path: `.env`,
+})
+
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
